8a76d9276a8479d292b17bb169a8278be21ac5ae
cybertekschool <github@cybertekschool.com>
2020-01-15 14:32:33 -0500

    login page

--- src/test/java/com/weborders/pages/BasePage.java
+++ src/test/java/com/weborders/pages/BasePage.java
@@ -1,4 +1,18 @@
 package com.weborders.pages;
 
+
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public abstract class BasePage {
+
+
+    public BasePage(){
+        //it's coming from selenium webdriver
+        //to enable @FindBy annotation
+        PageFactory.initElements(Driver.get(), this);
+    }
 }
+
+
--- src/test/java/com/weborders/pages/LoginPage.java
+++ src/test/java/com/weborders/pages/LoginPage.java
@@ -1,4 +1,32 @@
 package com.weborders.pages;
 
-public class LoginPage {
+import com.weborders.utilities.ConfigurationReader;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "ctl00_MainContent_username")
+    public WebElement userNameInputElement;
+
+    //shortcut for driver.findElement(By.id("ctl00_MainContent_password"))
+    @FindBy(id = "ctl00_MainContent_password")
+    public WebElement passwordInputElement;
+
+    public void login(){
+        String userName= ConfigurationReader.getProperty("user_name");
+        String password = ConfigurationReader.getProperty("password");
+
+        userNameInputElement.sendKeys(userName);
+        passwordInputElement.sendKeys(password, Keys.ENTER);
+        //Keys.ENTER instead of login click
+    }
+
+    public void login(String userName, String password){
+        userNameInputElement.sendKeys(userName);
+        passwordInputElement.sendKeys(password, Keys.ENTER);
+        //Keys.ENTER instead of login click
+    }
+
 }
