3152bb5a5cb0e818530d8697f195243cc78b513f
cybertekschool <github@cybertekschool.com>
2020-01-22 12:18:00 -0500

    added json plugin

--- src/test/java/com/weborders/runners/CucumberRunner.java
+++ src/test/java/com/weborders/runners/CucumberRunner.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/weborders/step_definitions",
+        plugin = "json:target/cucumber.json",
 //        tags = "@view_all_orders",
         dryRun = false
 )
